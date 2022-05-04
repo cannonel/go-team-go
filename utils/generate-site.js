819dc9f9d33b4798d0c1e13901679023ab0203ab
Erika Cannon <cannone.rva@gmail.com>
2022-05-04 15:52:31 -0400

    add generate-site file

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,37 +1,31 @@
-const fs = require('fs');
-
-// writing files
-const writeFile = fileContent => {
+const fs = require("fs");
+// const { resolve } = require('path');
+const writeFile = (fileContent) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile('./dist/index.html', fileContent, err => {
+    fs.writeFile("./dist/index.html", fileContent, (err) => {
       if (err) {
         reject(err);
         return;
       }
-
       resolve({
         ok: true,
-        message: 'File created!'
+        message: "File created!",
       });
     });
   });
 };
-
-// copying file
 const copyFile = () => {
   return new Promise((resolve, reject) => {
-    fs.copyFile('./src/style.css', './dist/style.css', err => {
+    fs.copyFile("src/style.css", "dist/index.html", (err) => {
       if (err) {
         reject(err);
         return;
       }
-
       resolve({
         ok: true,
-        message: 'Stylesheet created!'
+        message: "Style sheet copied",
       });
     });
   });
 };
-
 module.exports = { writeFile, copyFile };
